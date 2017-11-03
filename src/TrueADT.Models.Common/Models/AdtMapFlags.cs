8c8e0176d630d4724d41655b0c2e570e15a04ee8
HelloKitty <andrew.blakely@ymail.com>
2017-11-02 19:02:23 -0500

    Added AdtMapFlags

---
+++ src/TrueADT.Models.Common/Models/AdtMapFlags.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace TrueADT
+{
+	/// <summary>
+	/// Flags for optional data or behaviour that the ADT files contain.
+	/// Commonly called MHDRFlags in the SMMapHeader.
+	/// </summary>
+	[Flags]
+	[WireDataContract]
+	public enum AdtMapFlags
+	{
+		/// <summary>
+		/// Indicates that the ADT defines a bounding box for flying collision.
+		/// </summary>
+		FlyingBoundingBoxObject = 1,
+
+		//TODO: Explore what this is.
+		/// <summary>
+		/// Set on some Northrend maps. Unknown.
+		/// </summary>
+		Northrend = 2,
+	}
+}
